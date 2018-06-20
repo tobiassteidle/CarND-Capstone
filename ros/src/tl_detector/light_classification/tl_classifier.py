a6a2e537aad40f23176415427bdadf4f90d8c7f4
Tobias Steidle <tobias.steidle@softwaredev.de>
2018-06-20 07:25:26 +0200

    Fix in Class method

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -54,7 +54,7 @@ class TLClassifier(object):
 
         return switcher.get(classification, TrafficLight.UNKNOWN)
 
-    def load_image_into_numpy_array(image):
+    def load_image_into_numpy_array(self, image):
         (im_width, im_height) = image.size
         return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
